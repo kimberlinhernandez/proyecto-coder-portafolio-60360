54e9f7f9f49dc2741dd218a30703f2c0d135b31b
kimberlinhernandez <kimdhm17@gmail.com>
2024-03-06 21:05:28 -0600

    Agregar archivo de JS

---
+++ index.js
@@ -0,0 +1,8 @@
+const burgerMenu = document.querySelector (".burguer-menu");
+const navMenu = document.querySelector (".nav-menu");
+
+burgerMenu.addEventListener ("click", () => {
+        navMenu.classList.toggle("nav-menu-visible");
+    }); 
+
+    /**-- AQUÍ INTENTÉ CONECTAR EL BOTÓN Y TODAVÍA NO LOGRO DESCRIFRAR POR QUÉ NO FUNCIONA, AYUDA // Busqué tutoriales para poder hacer esta conexión, pero no lo logro--*/
