a9265eeeaf8da512a119e207a39acdbdb8bf4e98
kimberlinhernandez <kimdhm17@gmail.com>
2024-03-09 04:36:13 -0600

    nuevos cambios en el sitio

--- nav.js
+++ nav.js
@@ -5,3 +5,17 @@ function openNav (){
 function closeNav (){
     document.getElementById("mobile-menu").style.width = "0%";
 }
+
+const changingWordElement = document.getElementById("changing-word");
+const words = ["Copywriter...", "Redactora creativa...", "Profesional de marketing...", "Especialista en optimización de contenidos..."];
+
+let index = 0;
+
+function changeWord() {
+    changingWordElement.textContent = words[index];
+    index = (index + 1) % words.length;
+    setTimeout(changeWord, 5000); // Llama a la función nuevamente después de 10 segundos
+}
+
+// Llama a la función por primera vez después de un segundo
+setTimeout(changeWord, 1000);
