c12f1feaec69ef1c41ab81e1fd8eeee7719b666d
kimberlinhernandez <kimdhm17@gmail.com>
2024-03-09 00:48:43 -0600

    nuevos cambios en todas las páginas, además de incorporación de menú hamburguesa

--- index.js
+++
@@ -1,8 +0,0 @@
-const burgerMenu = document.querySelector (".burguer-menu");
-const navMenu = document.querySelector (".nav-menu");
-
-burgerMenu.addEventListener ("click", () => {
-        navMenu.classList.toggle("nav-menu-visible");
-    }); 
-
-    /**-- AQUÍ INTENTÉ CONECTAR EL BOTÓN Y TODAVÍA NO LOGRO DESCRIFRAR POR QUÉ NO FUNCIONA, AYUDA // Busqué tutoriales para poder hacer esta conexión, pero no lo logro--*/
